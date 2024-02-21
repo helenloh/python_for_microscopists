6c3147e1abf90daa06e0ac82e78816fca6c1ad05
helenloh <81956414+helenloh@users.noreply.github.com>
2024-02-21 22:09:16 +0800

    Update 042_data_analysis_using_Seaborn_Plotting.py
    add comment and plot all chart

--- 042_data_analysis_using_Seaborn_Plotting.py
+++ 042_data_analysis_using_Seaborn_Plotting.py
@@ -16,14 +16,15 @@ __license__ = "Feel free to copy, I appreciate if you acknowledge Python for Mic
 
 
 import pandas as pd
-df = pd.read_csv('manual_vs_auto.csv')
+df = pd.read_csv('other_files/manual_vs_auto.csv')
 
 import seaborn as sns
 
 ##############
 #Single variable (distribution histogram plots)
-#sns.distplot(df['Manual'])  #Will fail as we have a few missing values.
-
+sns.distplot(df['Manual'])  #Will fail as we have a few missing values.
+plt.title('seaborn')
+plt.show()
 #Let us fill missing values with a value of 100
 df['Manual'].fillna(100, inplace=True)
 
@@ -33,41 +34,47 @@ sns.distplot(df['Manual'])   #The overlay over histogram is KDE plot (Kernel den
 #KDE is a way to estimate the probability density function of a continuous random variable.
 
 import pandas as pd
-df = pd.read_csv('manual_vs_auto.csv')
+df = pd.read_csv('other_files/manual_vs_auto.csv')
 df['Manual'].fillna(100, inplace=True)
 
 import seaborn as sns
 sns.kdeplot(df['Manual'], shade=True)
+plt.title('kde plot')
+plt.show()
 
 ## Add Multiple plots
 sns.kdeplot(df['Auto_th_2'], shade=True)
 sns.kdeplot(df['Auto_th_3'], shade=True)
 sns.kdeplot(df['Auto_th_4'], shade=True)
+plt.title('kde2 plot')
+plt.show()
 ###################
 
 
 #Basic line plot
 import pandas as pd
-df = pd.read_csv('manual_vs_auto.csv')
+df = pd.read_csv('other_files/manual_vs_auto.csv')
 df['Manual'].fillna(100, inplace=True)
 
 import seaborn as sns
 sns.set(style='darkgrid')   #Adds a grid
 sns.lineplot(x='Image', y='Manual', data=df, hue='Unnamed: 0')   #Simple line plot
 #Hue tells seaborn how to color various subcategories, like our set in this example.
-
+plt.title('lineplot')
+plt.show()
 
 ##############################            
 #Scatter plots
 import pandas as pd
-df = pd.read_csv('manual_vs_auto.csv')
+df = pd.read_csv('other_files/manual_vs_auto.csv')
 df['Manual'].fillna(100, inplace=True)
-import seaborn as sns
 
 #Basic scatter plot
 sns.jointplot(x="Manual", y="Auto_th_2", data=df)
 #KDE plot, Kernel density estimation.
 sns.jointplot(x="Manual", y="Auto_th_2", data=df, kind="kde")
+plt.title('jointplot')
+plt.show()
 
 
 #Relationship between each feature and another selected feature can be easily plotted
@@ -76,28 +83,26 @@ sns.jointplot(x="Manual", y="Auto_th_2", data=df, kind="kde")
 import pandas as pd
 import seaborn as sns
 
-df = pd.read_csv('manual_vs_auto.csv')
+df = pd.read_csv('other_files/manual_vs_auto.csv')
 df['Manual'].fillna(100, inplace=True)
 print(df.columns)
 
-#sns.pairplot(df, x_vars=["Auto_th_2", "Auto_th_3", "Auto_th_4"], y_vars="Manual")
-
-#too small. Let us chage the size
-
 sns.pairplot(df, x_vars=["Auto_th_2", "Auto_th_3", "Auto_th_4"], y_vars="Manual", size=6, aspect=0.75)
-
+plt.title('pairplot')
+plt.show()
 
 #Scatterplot with linear regression
 
 import pandas as pd
-df = pd.read_csv('manual_vs_auto.csv')
+df = pd.read_csv('other_files/manual_vs_auto.csv')
 df['Manual'].fillna(100, inplace=True)
 #Change Unnamed: 0 name to Image_set
 df = df.rename(columns = {'Unnamed: 0':'Image_set'})
 import seaborn as sns
 
 sns.lmplot(x='Manual', y='Auto_th_2', data=df, hue='Image_set', order=1)  #Scatterplot with linear regression fit and 95% confidence interval
-
+plt.title('lmplot')
+plt.show()
 #If you want equation, not possible to display in seaborn but you can get it the
 #regular way using scipy stats module. 
 from scipy import stats
@@ -111,50 +116,41 @@ print(slope, intercept)
 #Swarm plots
 #Let's use manual_vs_auto2 file that we generated earlier 
 import pandas as pd
-df = pd.read_csv('manual_vs_auto2.csv')
+df = pd.read_csv('other_files/manual_vs_auto2.csv')
 df['Manual'].fillna(100, inplace=True)
 print(df.head())
 
 import seaborn as sns
-
-#sns.swarmplot(x = "Image_set", y="Manual", data = df, hue="cell_count_index")
-
-#SPlit each category
+import matplotlib.pyplot as plt
+sns.swarmplot(x = "Image_set", y="Manual", data = df, hue="cell_count_index")
+plt.title('seaborn1')
+plt.show()
+# #SPlit each category
 sns.swarmplot(x = "Image_set", y="Manual", data = df, hue="cell_count_index", dodge=True)
+plt.title('seaborn2')
+plt.show()
+# ##################
+# """
+# we can utilise the pandas Corr() to find the correlation between each variable 
+# in the matrix and plot this using Seaborn’s Heatmap function, 
+# specifying the labels and the Heatmap colour range.
+
+# """
 
 
-##################
-"""
-we can utilise the pandas Corr() to find the correlation between each variable 
-in the matrix and plot this using Seaborn’s Heatmap function, 
-specifying the labels and the Heatmap colour range.
+# import pandas as pd
+# df = pd.read_csv('other_files/manual_vs_auto.csv')
+# print(df.dtypes)
+# df['Manual'].fillna(100, inplace=True)
+# #Change Unnamed: 0 name to Image_set
+# df = df.rename(columns = {'Unnamed: 0':'Image_set'})
 
-"""
 
-
-import pandas as pd
-df = pd.read_csv('manual_vs_auto.csv')
-print(df.dtypes)
-df['Manual'].fillna(100, inplace=True)
-#Change Unnamed: 0 name to Image_set
-df = df.rename(columns = {'Unnamed: 0':'Image_set'})
+# print("============================================================")
 import seaborn as sns
 corr = df.loc[:,df.dtypes == 'int64'].corr() #Correlates all int64 columns
 sns.heatmap(corr, xticklabels=corr.columns, yticklabels=corr.columns, cmap=sns.diverging_palette(220, 10, as_cmap=True))
-##########################
-
-
-
-
-
-
-
-
-
-
-
-
-
+# ##########################
 
 
 
